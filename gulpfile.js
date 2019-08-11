4b8f879542c903ce213b58c9babf9350c0743282
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-08-11 12:04:05 -0300

    Primeiro commit do tema

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,23 @@
-var gulp                = require('gulp'),
-svgSprite               = require('gulp-svg-sprite'),
-clipboard               = require("gulp-clipboard"),
-stylus                  = require('gulp-stylus'),
-svgmin                  = require('gulp-svgmin'),
-gulpif                  = require('gulp-if'),
-postcss                 = require('gulp-postcss'),
-autoprefixer            = require('autoprefixer'),
-pxtorem                 = require('postcss-pxtorem'),
-browserSync             = require('browser-sync').create(),
-path                    = require('path'),
-backstop                = require('backstopjs'),
-url                     = require('url')
+var gulp = require('gulp'),
+  svgSprite = require('gulp-svg-sprite'),
+  clipboard = require("gulp-clipboard"),
+  stylus = require('gulp-stylus'),
+  svgmin = require('gulp-svgmin'),
+  gulpif = require('gulp-if'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  pxtorem = require('postcss-pxtorem'),
+  browserSync = require('browser-sync').create(),
+  path = require('path'),
+  backstop = require('backstopjs'),
+  url = require('url')
 
 var config = require('./skin');
-var {rootValue, unitPrecision} = config.pxtorem;
-var {protocol, host, theme } = config;
+var { rootValue, unitPrecision } = config.pxtorem;
+var { protocol, host, theme } = config;
 
 var processors = [
   autoprefixer({
-    browsers: config.browsers,
     grid: true,
     cascade: false
   }),
@@ -46,27 +45,27 @@ var processors = [
   })
 ];
 
-function loadbrowserSync(){
+function loadbrowserSync() {
   browserSync.init({
     port: 8080,
-    proxy : [protocol, host].join('://'),
+    proxy: [protocol, host].join('://'),
     serveStatic: require('./server/static'),
     middleware: require('./server/routes')
-  }, function(){
+  }, function () {
     console.log('Altere o arquivo css/theme/general.styl para testar');
   });
 }
 
 gulp.task('svg-min', function () {
   gulp.src('svg/*.svg').pipe(svgmin({
-    plugins: [{removeStyleElement: true}]
+    plugins: [{ removeStyleElement: true }]
   })).pipe(gulp.dest('./svg'));
 });
 
-gulp.task('svg', function() {
+gulp.task('svg', function () {
   // More complex configuration example
   const config = {
-    svg:{
+    svg: {
       xmlDeclaration: false,
       doctypeDeclaration: false,
       rootAttributes: {
@@ -74,8 +73,8 @@ gulp.task('svg', function() {
         id: 'skin-sprite'
       }
     },
-    mode:{
-      symbol:{
+    mode: {
+      symbol: {
         symbol: true,
         inline: false
       }
@@ -88,54 +87,54 @@ gulp.task('svg', function() {
     .pipe(clipboard());
 });
 
-gulp.task('prod', function() {
+gulp.task('prod', function () {
   gulp.src('./css/*.styl')
     .pipe(stylus({
       'include css': true,
       'import': 'vars/prod',
       'url': {
-        'name':'embedurl',
+        'name': 'embedurl',
         'limit': false
       }
     }))
     .pipe(gulp.dest('./css'));
-    gulp.src('css/one.css').pipe(clipboard());
+  gulp.src('css/one.css').pipe(clipboard());
 });
 
-gulp.task('postcss', function() {
-  gulp.src('./css/*.css').pipe(postcss(processors)).pipe(gulp.dest('./css')).pipe(browserSync.reload({ stream: true}));
+gulp.task('postcss', function () {
+  gulp.src('./css/*.css').pipe(postcss(processors)).pipe(gulp.dest('./css')).pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('dev', function() {
+gulp.task('dev', function () {
   gulp.src('./css/*.styl')
     .pipe(stylus({
       'include css': true,
-      'import'     : 'vars/dev',
+      'import': 'vars/dev',
       'url': {
-        'name':'embedurl',
+        'name': 'embedurl',
         'limit': false
       }
     }))
     .pipe(gulp.dest('./css'))
 });
 
-gulp.task('copy-css', function() {
+gulp.task('copy-css', function () {
   gulp.src("./css/one.css")
-      .pipe(clipboard())
-      .pipe(gulp.dest("./dist"));
+    .pipe(clipboard())
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   var stylus = gulp.watch('**/*.styl', ['dev']);
   var css = gulp.watch('./css/*.css', ['postcss']);
   loadbrowserSync();
 
-  stylus.on('change', function(event) {
+  stylus.on('change', function (event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
   var stylus = gulp.watch('./css/*.styl', ['dev']);
 
-  css.on('change', function(event) {
+  css.on('change', function (event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
 });
