48b41a1676957c2e6ebeb1149da563c3bb7b6665
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-09-16 10:05:40 -0300

    Add ScrollTop JS - Funcion add class in body scrolling page

--- js/one.js
+++ js/one.js
@@ -1415,10 +1415,37 @@ Para cada SVG adicionado, adicione um objeto com os parametros:
  * 'scrollStop'
  * 'ajaxComplete'
  */
+function scrollTop() {
+    var currentScrolling = 0
+
+    $j(window).scroll(function() {
+        var body = $j('body')
+        var scrollTop = $j(window).scrollTop()
+        var heightHeader = $j('.header-container').outerHeight()
+
+        if (scrollTop > 0) body.addClass('scrolling')
+        else body.removeClass('scrolling')
+
+        if (scrollTop > heightHeader) {
+            if (scrollTop > currentScrolling) {
+                body.addClass('scrolling--down').removeClass('scrolling--up')
+            } else {
+                body.addClass('scrolling--up').removeClass('scrolling--down')
+            }
+            currentScrolling = scrollTop
+        } else {
+            body.removeClass('scrolling--up').removeClass('scrolling--down')
+        }
+    })
+}
 
 $j(document)
     .ready(function($) {
         // document.ready
+        // Scrolling
+        scrollTop()
+
+        // Menu Categories
         $('.categories .parent').click(function(event) {
             if ($(event.target).hasClass('parent')) {
                 $(event.target).toggleClass('on')
